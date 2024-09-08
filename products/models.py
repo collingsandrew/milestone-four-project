73b3a44100bc557a4da8b3d1bf7ad7213f52ad98
Andrew Collins <andrewcollings@outlook.com>
2024-09-08 15:45:31 +0000

    Add product and category models to products app

--- products/models.py
+++ products/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=100, unique=True, blank=True, null=True)
+    isbn = models.CharField(max_length=13, unique=True, blank=True, null=True)
+    format_type = models.CharField(max_length=50, blank=True, null=True)
+    author = models.CharField(max_length=254, blank=True, null=True)
+    publisher = models.CharField(max_length=254, blank=True, null=True)
+    publication_date = models.DateField(blank=True, null=True)
+    description = models.TextField()
+    edition = models.CharField(max_length=100, blank=True, null=True)
+    language = models.CharField(max_length=50, blank=True, null=True)
+    num_of_pages = models.IntegerField(blank=True, null=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    average_rating = models.DecimalField(max_digits=3, decimal_places=2, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(default=0)
+    weight = models.IntegerField(blank=True, null=True)
+    dimensions = models.CharField(max_length=254, blank=True, null=True)
+    is_favourite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
