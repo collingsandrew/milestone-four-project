85e841702ac4d166c34878fd72dc43cd733c5cbe
Andrew Collins <andrewcollings@outlook.com>
2024-09-10 17:17:23 +0000

    Add unit test for products page view

---
+++ products/test_views.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+
+class TestProductViews(TestCase):
+
+    def test_get_products_page(self):
+        """
+        test get products page
+        """
+        response=self.client.get('/products/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/products.html')
