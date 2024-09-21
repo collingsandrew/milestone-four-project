60a2257a32d03ac1d6cba0d0d8920b7477a542c9
Andrew Collins <andrewcollings@outlook.com>
2024-09-21 15:32:23 +0000

    Fix update and remove bag product javascript to select all specified elements instead of only one

--- static/js/base.js
+++ static/js/base.js
@@ -3,32 +3,36 @@
 /* credit: Code Institute - Boutique Ado project */
 
 // update product quantity
-document.querySelector('.update-link').addEventListener('click', function(e) {
-    let form = this.previousElementSibling;
-    
-    if (form.classList.contains('update-form')) {
-        form.submit();
-    }
+document.querySelectorAll('.update-link').forEach(function(updateLink) {
+    updateLink.addEventListener('click', function(e) {
+        let form = this.previousElementSibling;
+        
+        if (form.classList.contains('update-form')) {
+            form.submit();
+        }
+    });
 });
 
 // remove product from bag
-document.querySelector('.remove-item').addEventListener('click', function(e) {
-    let csrfToken = "{{ csrf_token }}";
-    let itemId = this.id.split('remove_')[1];
-    let url = `/bag/remove/${itemId}`;
-    let data = new FormData();
-    data.append('csrfmiddlewaretoken', csrfToken);
+document.querySelectorAll('.remove-item').forEach(function(removeItem) {
+    removeItem.addEventListener('click', function(e) {
+        let csrfToken = document.querySelector('input[name=csrfmiddlewaretoken]').value;
+        let itemId = this.id.split('remove_')[1];
+        let url = `/bag/remove/${itemId}/`;
+        let data = new FormData();
+        data.append('csrfmiddlewaretoken', csrfToken);
 
-    fetch(url, {
-        method: 'POST',
-        body: data
-    })
-    .then(response => {
-        if (response.ok) {
-            location.reload();
-        } else {
-            console.error('Error removing item');
-        }
-    })
-    .catch(error => console.error('Error:', error));
+        fetch(url, {
+            method: 'POST',
+            body: data
+        })
+        .then(response => {
+            if (response.ok) {
+                location.reload();
+            } else {
+                console.error('Error removing item');
+            }
+        })
+        .catch(error => console.error('Error:', error));
+    });
 });
